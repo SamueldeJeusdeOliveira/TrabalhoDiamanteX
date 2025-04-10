7e7ad6379a0f4c58ca41a46bed55753aa23321f8
SamueldeJeusDeOliveira <samuksjj@gmail.com>
2025-04-10 17:51:05 -0300

    Testando o método VerificaImpar e atualizando o README.md

--- TrabalhoDiamanteX/Program.cs
+++ TrabalhoDiamanteX/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            
+            // Menu para o usuário
+            Console.WriteLine("Bem-Vindo ao nosso gerador de diamantes!" + '\n');
+            Console.WriteLine("------------------------------------------------------" + '\n');
+            Console.WriteLine("Primeiramente para calcularmos e gerarmos um diamante precisaremos de um número ímpar inteiro!");
+            Console.Write("Por isso digite um número ímpar inteiro por favor(EXEMPLO: 1,3,5,7): ");
+            int n = int.Parse(Console.ReadLine());
+            n = VerificaImpar(n);
+            Console.WriteLine("------------------------------------------------------" + '\n');
+            Console.WriteLine(n);
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        static int VerificaImpar(int n)
+        {
+            while (n % 2 == 0)
+            {
+                Console.WriteLine("Número inválido! Tente novamente com um número ímpar:");
+                n = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Número válido!");
+            return n;
         }
     }
 }
